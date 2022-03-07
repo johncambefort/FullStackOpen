ae86076fd13b4b455af34c7e2107432a88a34d51
John Cambefort <jcambefort@middlebury.edu>
2022-03-07 15:39:46 -0500

    1.6-unicafe

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,67 +1,39 @@
-const Header = (props) => <h1>{props.course.name}</h1>;
+import { useState } from 'react'
 
-const Part = (props) => {
-  return (
-    <p>
-      {props.part} {props.exercises}
-    </p>
-  );
-};
+const Header = (props) => <h1>{props.text}</h1>;
 
-const Content = (props) => {
-  return (
-    <div>
-      <Part
-        part={props.course.parts[0].name}
-        exercises={props.course.parts[0].exercises}
-      />
-      <Part
-        part={props.course.parts[1].name}
-        exercises={props.course.parts[1].exercises}
-      />
-      <Part
-        part={props.course.parts[2].name}
-        exercises={props.course.parts[2].exercises}
-      />
-    </div>
-  );
-};
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>;
 
-const Total = (props) => (
-  <p>
-    Number of exercises 
-    {props.course.parts[0].exercises +
-      props.course.parts[1].exercises +
-      props.course.parts[2].exercises}
-  </p>
-);
+const Feedback = ({text, numVotes}) => <p>{text} {numVotes}</p>;
+
+const Statistics = ({good, neutral, bad}) => {
+  return (<>
+    <Feedback text="good" numVotes={good} />
+    <Feedback text="neutral" numVotes={neutral} />
+    <Feedback text="bad" numVotes={bad} />
+  </>);
+}
 
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
-  };
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const increaseGoodByOne = () => setGood(good + 1);
+  const increaseNeutralByOne = () => setNeutral(neutral + 1);
+  const increaseBadByOne = () => setBad(bad + 1);
 
   return (
     <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
+      <Header text="give feedback" />
+      <Button onClick={increaseGoodByOne} text="good"/>
+      <Button onClick={increaseNeutralByOne} text="neutral"/>
+      <Button onClick={increaseBadByOne} text="bad"/>
+      <Header text="statistics" />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App

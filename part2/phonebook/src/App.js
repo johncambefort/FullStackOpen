633435acd7b935aa6690c764db32478eabb29a97
John Cambefort <jcambefort@middlebury.edu>
2022-03-08 15:52:10 -0500

    2.9-phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,18 +2,22 @@ import { useState } from "react";
 import Person from "./components/Person";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ 
-    name: "Arto Hellas",
-    phone: "(802) 771-5447"
-  }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', phone: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', phone: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', phone: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', phone: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState("");
   const [newPhone, setNewPhone] = useState("")
+  const [newFilter, setNewFilter] = useState("");
 
   const addPerson = (event) => {
     event.preventDefault();
     const personObj = { 
       name: newName,
-      phone: newPhone
+      phone: newPhone,
+      id: persons.length + 1
      }; // Construct a new Person object
 
     if (persons.find((p) => p.name === personObj.name)) {
@@ -31,9 +35,21 @@ const App = () => {
     setNewPhone(event.target.value);
   }
 
+  const handleNewFilter = (event) => {
+    setNewFilter(event.target.value);
+  }
+
+  const personsToShow = (newFilter === "")
+  ? persons
+  : persons.filter((p) => p.name.toLowerCase().includes(newFilter.toLowerCase()));
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with: <input value={newFilter} onChange={handleNewFilter} />
+      </div>
+      <h4>Add new contact</h4>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNewName} />
@@ -47,8 +63,8 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person) => (
-          <Person key={person.name} person={person} />
+        {personsToShow.map((person) => (
+          <Person key={person.id} person={person} />
         ))}
       </ul>
     </div>

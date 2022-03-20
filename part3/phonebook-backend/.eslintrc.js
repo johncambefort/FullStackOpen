1901595ee679aefa4f6fa747cca764d15237d279
John Cambefort <jcambefort@middlebury.edu>
2022-03-20 16:25:55 -0400

    Add linter file

---
+++ part3/phonebook-backend/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    eqeqeq: "error",
+    "no-trailing-spaces": "error",
+    "object-curly-spacing": ["error", "always"],
+    "arrow-spacing": ["error", { before: true, after: true }],
+    "no-console": 0,
+    "no-unused-vars": 0,
+    "no-undef": 0 // added these bc they were giving me issues with "require"
+  },
+};
